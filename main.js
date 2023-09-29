c0e7a3be76dd8731f332ae06a8d78455e3ebaa0a
unknown <andresvashernandez@outlook.com>
2023-09-29 14:59:23 -0500

    first changes responsive nav

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
+const menuIcon = document.querySelector("#menu-icon");
+const barMenu = document.querySelector(".Navigation");
 
-d
+menuIcon.addEventListener("click",function(){
+    if(barMenu.classList.contains("oculto")){
+        barMenu.classList.remove("oculto");
+    }else{
+        barMenu.classList.add("oculto")
+    }
+})
